74063e8d6e9f2fbe58a90756aef22f43142c9bcd
Massim0o4 <massimo.iannetti@studio.unibo.it>
2026-08-20 23:47:29 +0200

    Initial project structure

---
+++ vector2.cpp
@@ -0,0 +1,30 @@
+#include "vector2.hpp"
+
+#include <cmath>
+
+namespace boids {
+
+Vector2 operator+(Vector2 const& l, Vector2 const& r) {
+  return Vector2{l.x + r.x, l.y + r.y};
+}
+
+Vector2 operator-(Vector2 const& l, Vector2 const& r) {
+    return Vector2{l.x + r.x, l.y + r.y};
+}
+
+Vector2 operator*(Vector2 const& l, double scalar) {
+    return Vector2{l.x * scalar , l.y * scalar };
+}
+
+Vector2 operator/(Vector2 const& l, double scalar) {
+    return Vector2{l.x / scalar, l.y / scalar};
+}
+double norm(Vector2 const& v) {
+    return  std::sqrt ( v.x * v.x + v.y * v.y );
+}
+double distance(Vector2 const& a, Vector2 const& b) {
+    Vector2 diff = a - b;
+    return norm (diff);
+}
+
+}
---
+++ vector2.hpp
@@ -0,0 +1,18 @@
+#ifndef VECTOR2_HPP
+#define VECTOR2_HPP
+
+namespace boids {
+struct Vector2 {
+  double x;
+  double y;
+};
+Vector2 operator+(Vector2 const& l, Vector2 const& r);
+Vector2 operator-(Vector2 const& l, Vector2 const& rhs);
+Vector2 operator*(Vector2 const& v, double scalar);
+Vector2 operator/(Vector2 const& v, double scalar);
+
+double norm(Vector2 const& v);
+double distance(Vector2 const& a, Vector2 const& b);
+}
+
+#endif
